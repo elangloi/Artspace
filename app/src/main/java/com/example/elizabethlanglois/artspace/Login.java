c22b9b20f837cebc7b0aba8077f37a10323cb972
Alex Ma <alex555ma@gmail.com>
2018-11-12 00:02:26 -0500

    renamed monaLisa file to monalisa to bypass debug error, still need to fix debug, also added simple login screen code (not tested) (caches may be different, please be aware)

---
+++ app/src/main/java/com/example/elizabethlanglois/artspace/Login.java
@@ -0,0 +1,48 @@
+package com.example.elizabethlanglois.artspace;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity{
+
+    Button b1;
+    EditText username;
+    EditText password;
+
+    //placeholder database for now
+    private String [] usernames = {"User1"};
+    private String [] passwords = {"Pass1"};
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_login);
+
+        b1 = (Button) findViewById(R.id.button1);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //placeholder check for now
+                if(username.getText().toString().equals(usernames[0]) && password.getText().toString().equals(passwords[0])){
+                    Log.d("Success", "Got through fine");
+                    //correct password
+                }else{
+                    Log.d("Failure", "Failed");
+                    //wrong password
+                }
+            }
+
+        });
+    }
+
+
+
+}
