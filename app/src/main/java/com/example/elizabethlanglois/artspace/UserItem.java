f54b9cada6fb629f61863ec600cda2831f88d218
elizabethlanglois <elizabethclanglois@gmail.com>
2018-11-29 14:25:57 -0500

    useritem

---
+++ app/src/main/java/com/example/elizabethlanglois/artspace/UserItem.java
@@ -0,0 +1,14 @@
+package com.example.elizabethlanglois.artspace;
+
+import java.util.List;
+
+public class UserItem {
+
+    public String username, password;
+    public List<Double> favorites, created_art;
+
+    public UserItem() {
+        // For firebase
+    }
+
+}
